075f9366ef051cfc13286e156ea1c8ed222ff799
Bianca Pilegi <bpilegi@mobydigital.com>
2021-03-18 11:35:48 -0300

    Creando entidad Benefit

--- src/main/java/com/bianca/ejercicios/model/Benefit.java
+++ src/main/java/com/bianca/ejercicios/model/Benefit.java
@@ -1,2 +1,31 @@
-package com.bianca.ejercicios.model;public class Benefit {
+package com.bianca.ejercicios.model;
+
+import com.bianca.ejercicios.model.enums.TypeBenefit;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "benefits")
+public class Benefit {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private TypeBenefit type;
+
+    @Column(name = "discount_amount")
+    private double discountAmount;
+
+    @Column(name = "foreign_key")
+    private Long foreignKey;
 }
