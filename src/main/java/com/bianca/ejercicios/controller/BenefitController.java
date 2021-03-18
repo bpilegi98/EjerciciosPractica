d7b806d70faadb78e18057b8d518c7bca4361076
Bianca Pilegi <bpilegi@mobydigital.com>
2021-03-18 11:54:47 -0300

    Creando controladora de Benefit

--- src/main/java/com/bianca/ejercicios/controller/BenefitController.java
+++ src/main/java/com/bianca/ejercicios/controller/BenefitController.java
@@ -1,2 +1,26 @@
-package com.bianca.ejercicios.controller;public class BenefitController {
+package com.bianca.ejercicios.controller;
+
+import com.bianca.ejercicios.model.Benefit;
+import com.bianca.ejercicios.service.BenefitService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ejercicio")
+public class BenefitController {
+
+    private BenefitService benefitService;
+
+    public BenefitController(BenefitService benefitService) {
+        this.benefitService = benefitService;
+    }
+
+    @GetMapping("/1")
+    public List<Benefit> exerciseOne() throws JsonProcessingException {
+        return benefitService.convert();
+    }
 }
