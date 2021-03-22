2873fa811f647b91ef319d51235166b9a1f20fd5
Bianca Pilegi <bpilegi@mobydigital.com>
2021-03-22 10:27:10 -0300

    Volviendo a agregar anotaciones de persistencia en entidad Benefit

--- src/main/java/com/bianca/ejercicios/model/Benefit.java
+++ src/main/java/com/bianca/ejercicios/model/Benefit.java
@@ -8,18 +8,31 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Data
+@Entity
+@Table(name = "benefits")
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Benefit {
 
+    @Id
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     private TypeBenefit type;
 
+    @Column(name ="discount_amount")
     private double discountAmount;
 
+    @Column(name = "foreign_key")
     private Long foreignKey;
 }
