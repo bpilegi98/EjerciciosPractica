35634051afbe3b9568522f4a4efa693696016711
Bianca Pilegi <bpilegi@mobydigital.com>
2021-03-22 10:18:21 -0300

    Cambiando JpaRepository por CrudRepository

--- src/main/java/com/bianca/ejercicios/repository/BenefitRepository.java
+++ src/main/java/com/bianca/ejercicios/repository/BenefitRepository.java
@@ -1,9 +1,9 @@
 package com.bianca.ejercicios.repository;
 
 import com.bianca.ejercicios.model.Benefit;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BenefitRepository extends JpaRepository<Benefit, Long> {
+public interface BenefitRepository extends CrudRepository<Benefit, Long> {
 }
